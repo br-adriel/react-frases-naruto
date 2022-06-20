04dd4cb06b067428a01746348418d6a29c26aca1
Adriel <adriel.fariasan@gmail.com>
2022-06-19 22:39:10 -0300

    Adiciona css global básico

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,5 +1,16 @@
-const GlobalStyle = () => {
-  return <></>;
-};
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }  
+
+  body {
+    color: #333c36;
+    font-family: 'Patua One', serif;
+  }
+`;
 
 export default GlobalStyle;
