8f2e43b90db101885d7bfc2569ab7efdd86d400b
Adriel <adriel.fariasan@gmail.com>
2022-06-19 22:04:23 -0300

    Define estrutura inicial de arquivos

---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,5 @@
+const GlobalStyle = () => {
+  return <></>;
+};
+
+export default GlobalStyle;
---
+++ src/pages/app/App.js
@@ -0,0 +1,10 @@
+import GlobalStyle from '../../components/GlobalStyle';
+
+export function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello</h1>
+    </>
+  );
+}
