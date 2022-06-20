1b8bfb1c9f4ca33ddbf4b21815064e7dbbc39227
Adriel <adriel.fariasan@gmail.com>
2022-06-19 22:46:02 -0300

    Implementa teste de renderizacao de citacao

--- src/pages/app/App.test.js
+++ src/pages/app/App.test.js
@@ -7,4 +7,10 @@ describe('renders the App', () => {
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
   });
+
+  test('it renders a quote paragraph', () => {
+    render(<App />);
+    const quote = screen.getByRole('paragraph');
+    expect(quote).toBeInTheDocument();
+  });
 });
