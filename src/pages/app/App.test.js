2e03f0a1fdcceb073a2edc22cc5295c4da3683a9
Adriel <adriel.fariasan@gmail.com>
2022-06-19 22:47:00 -0300

    Implementa teste de renderizacao de imagem

--- src/pages/app/App.test.js
+++ src/pages/app/App.test.js
@@ -13,4 +13,10 @@ describe('renders the App', () => {
     const quote = screen.getByRole('paragraph');
     expect(quote).toBeInTheDocument();
   });
+
+  test('it renders a image', () => {
+    render(<App />);
+    const image = screen.getByRole('img');
+    expect(image).toBeInTheDocument();
+  });
 });
