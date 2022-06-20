11473f19937c587b5bffe36d30e7664ede24a848
Adriel <adriel.fariasan@gmail.com>
2022-06-19 23:20:26 -0300

    Renomeia conjunto de testes de App

--- src/pages/app/App.test.js
+++ src/pages/app/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { App } from './App';
 
-describe('renders the App', () => {
+describe('it renders the App', () => {
   test('it renders a button', () => {
     render(<App />);
     const button = screen.getByRole('button');
