17ea8db1deae34c2a20c19fe8d3b9bc84a39fedb
Adriel <adriel.fariasan@gmail.com>
2022-06-21 22:18:32 -0300

    Adiciona foto do naruto

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,13 +1,17 @@
 import GlobalStyle from '../../components/GlobalStyle';
+import narutoPng from '../../images/naruto.png';
 
 export function App() {
   return (
     <>
       <GlobalStyle />
       <div>
-        <p className='quote'>Citação:</p>
+        <p>
+          <q>Carregando...</q>
+          <span>-</span>
+        </p>
         <button>Quote No Jutsu</button>
-        <img src='' alt='' />
+        <img src={narutoPng} alt='Naruto smiling with an eye closed' />
       </div>
     </>
   );
