4d980932d2baf470251af2dbf4dc68dbdf6ae5d9
Adriel <adriel.fariasan@gmail.com>
2022-06-21 22:48:14 -0300

    Adiciona css básico aos elementos

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,18 +1,58 @@
 import GlobalStyle from '../../components/GlobalStyle';
 import narutoPng from '../../images/naruto.png';
+import styled from 'styled-components';
 
 export function App() {
   return (
     <>
       <GlobalStyle />
-      <div>
-        <p>
-          <q>Carregando...</q>
-          <span>-</span>
-        </p>
-        <button>Quote No Jutsu</button>
-        <img src={narutoPng} alt='Naruto smiling with an eye closed' />
-      </div>
+      <Container>
+        <Content>
+          <Quotation>
+            <Quote>Carregando...</Quote>
+            <Author>-</Author>
+          </Quotation>
+          <button>Quote No Jutsu</button>
+        </Content>
+        <NarutoImg src={narutoPng} alt='Naruto smiling with an eye closed' />
+      </Container>
     </>
   );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  padding: 0 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+const NarutoImg = styled.img`
+  height: 85%;
+  align-self: flex-end;
+`;
+
+const Content = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+`;
+
+const Quotation = styled.p`
+  width: 100%;
+  diplay: flex;
+  flex-direction: column;
+`;
+
+const Author = styled.span`
+  margin-right: 30px;
+  margin-left: auto;
+`;
+
+const Quote = styled.q`
+  font-size: 1.5rem;
+  width: 100%;
+`;
