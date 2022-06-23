6a74544cc3169bea374bf0b0f20285003370491c
Adriel <adriel.fariasan@gmail.com>
2022-06-22 22:10:40 -0300

    Implementa mudanca de frase ao clicar botao

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -3,15 +3,27 @@ import narutoPng from '../../images/naruto.png';
 import styled from 'styled-components';
 import Quote from '../../components/Quote';
 import Button from '../../components/Button';
+import { getQuote } from '../../services';
+import { useState } from 'react';
 
 export function App() {
+  const [quote, setQuote] = useState('Carregando...');
+  const [author, setAuthor] = useState('Carregando...');
+
+  const loadQuote = async () => {
+    const quoteObj = await getQuote();
+
+    setQuote(quoteObj.quote);
+    setAuthor(quoteObj.speaker);
+  };
+
   return (
     <>
       <GlobalStyle />
       <Container>
         <Content>
-          <Quote />
-          <Button>Quote No Jutsu</Button>
+          <Quote text={quote} author={author} />
+          <Button onClk={loadQuote}>Quote No Jutsu</Button>
         </Content>
         <NarutoImg src={narutoPng} alt='Naruto smiling with an eye closed' />
       </Container>
