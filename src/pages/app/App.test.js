ec37c9a93cbbeef92edc401b5c605d89e2e1b004
Adriel <adriel.fariasan@gmail.com>
2022-06-22 21:57:17 -0300

    Implementa teste de funcionamento do App

--- src/pages/app/App.test.js
+++ src/pages/app/App.test.js
@@ -1,5 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { App } from './App';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+
+const response = { speaker: 'Speaker', quote: 'Test quote' };
+const server = setupServer(
+  rest.get(process.env.REACT_APP_API, (req, res, ctx) => {
+    return res(ctx.json(response));
+  })
+);
 
 describe('it renders the App', () => {
   test('it renders a button', () => {
@@ -12,5 +21,21 @@ describe('it renders the App', () => {
     render(<App />);
     const image = screen.getByAltText(/naruto/i);
     expect(image).toBeInTheDocument();
+  });
+});
+
+describe('The app works correctly', () => {
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  test('it updates the quote when button is clicked', async () => {
+    render(<App></App>);
+
+    const btn = screen.getByRole('button');
+    fireEvent.click(btn);
+
+    const quote = await screen.findByText(/test quote/i);
+    expect(quote).toBeInTheDocument();
   });
 });
